19f4412e494cf15790e5491f88c6fbe05d399ad8
kowalsska <kowalsska2@gmail.com>
2023-01-30 00:13:57 -1000

    Absolute pain

--- App.js
+++ App.js
@@ -1,19 +1,84 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import * as React from 'react';
+import { View, Text, Button , Image, ScrollView} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const App = () => {
+const logo = {
+  uri: 'https://reactnative.dev/img/tiny_logo.png',
+  width: 64,
+  height: 64,
+};
+
+function HomeScreen() {
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        height: 100,
-        padding: 20,
-      }}>
-      <View style={{backgroundColor: 'blue', flex: 0.3}} />
-      <View style={{backgroundColor: 'red', flex: 0.5}} />
-      <Text>Hello there!</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
-};
+}
+
+function SurfScreen() {
+  return (
+    <ScrollView>
+        <Text style={{fontSize: 96}}>Scroll me plz</Text>
+        <Image source={logo} />
+        <Image source={logo} />
+        <Image source={logo} />
+        <Image source={logo} />
+        <Image source={logo} />
+        <Text style={{fontSize: 96}}>If you like</Text>
+        <Image source={logo} />
+        <Image source={logo} />
+        <Image source={logo} />
+        <Image source={logo} />
+        <Image source={logo} />
+        <Text style={{fontSize: 96}}>Scrolling down</Text>
+        <Image source={logo} />
+        <Image source={logo} />
+        <Image source={logo} />
+        <Image source={logo} />
+        <Image source={logo} />
+        <Text style={{fontSize: 96}}>What's the best</Text>
+        <Image source={logo} />
+        <Image source={logo} />
+        <Image source={logo} />
+        <Image source={logo} />
+        <Image source={logo} />
+        <Text style={{fontSize: 96}}>Framework around?</Text>
+        <Image source={logo} />
+        <Image source={logo} />
+        <Image source={logo} />
+        <Image source={logo} />
+        <Image source={logo} />
+        <Text style={{fontSize: 80}}>React Native</Text>
+      </ScrollView>
+  );
+}
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={SurfScreen}
+        options={{ title: 'My home' }}
+      />
+      <Stack.Screen
+        name="Profile"
+        component={SurfScreen}
+        options={({ route }) => ({ title: route.params.name })}
+      />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
